57b0cbc27b24b02e628308233c91aaff5b3f61d6
Ben <golds349@morris.umn.edu>
2020-04-20 12:38:18 -0500

    Change note type to type used in iteration 2 drop table teams
    Completely change the Note type to match that used in the drop table teams code base rather than the one used in Team Rocket, which our codebase was otherwise based on.

--- server/src/main/java/umm3601/notes/Note.java
+++ server/src/main/java/umm3601/notes/Note.java
@@ -1,14 +1,41 @@
 package umm3601.notes;
 
-import org.mongojack.Id;
-import org.mongojack.ObjectId;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.mongodb.lang.Nullable;
+
+import org.bson.types.ObjectId;
+import org.mongojack.Id;
+
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Note {
 
-  @ObjectId @Id
+  @org.mongojack.ObjectId @Id
   public String _id;
+
   public String owner_id;
+
   public String body;
-  public boolean posted;
+
+  //public Date addDate = new Date();
+
+  @JsonGetter("addDate")
+  public Date getAddDate () {
+    return new ObjectId(_id).getDate();
+  }
+
+  // The annotations appear to contradict here, but they don't.
+  // What they're saying is, "It's totally fine if this property is null,
+  // but if it is we shouldn't serialize it into the document."
+  @Nullable @JsonInclude(Include.NON_NULL)
+  public String expireDate;
+
+  public String status;
 
 }
