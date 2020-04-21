766b7205f32951c6a08ec94c10688dc80f4c9122
Joe Walbran <walbr037@morris.umn.edu>
2020-04-20 22:54:13 -0500

    Write simple Karma tests for AddNoteComponent
    These are just tests to make sure that the AddNoteComponent can be instantiated.
    
    They are currently failing. ❓❓🤨
    
    I think the issue has to do with how we're handling observables—specifically, the userProfile$ field of AuthService.

--- client/src/app/add/add-note.component.spec.ts
+++ client/src/app/add/add-note.component.spec.ts
@@ -1,66 +1,75 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-// import { FormsModule, NgForm, ReactiveFormsModule, FormGroup, AbstractControl } from '@angular/forms';
-// import { MatCardModule } from '@angular/material/card';
-// import { MatFormFieldModule } from '@angular/material/form-field';
-// import { MatInputModule } from '@angular/material/input';
-// import { MatSelectModule } from '@angular/material/select';
-// import { MatSnackBarModule } from '@angular/material/snack-bar';
-// import { By } from '@angular/platform-browser';
-// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-// import { RouterTestingModule } from '@angular/router/testing';
-// import { MockNoteService } from 'src/testing/note.service.mock';
-// import { AddNoteComponent } from './add-note.component';
-// import { NotesService } from '../notes.service';
-// import { OwnerService } from '../owner.service';
-// import { MockOwnerService } from 'src/testing/owner.service.mock';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, NgForm, ReactiveFormsModule, FormGroup, AbstractControl } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { By } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MockNoteService } from 'src/testing/note.service.mock';
+import { AddNoteComponent } from './add-note.component';
+import { NotesService } from '../notes.service';
+import { OwnerService } from '../owner.service';
+import { MockOwnerService } from 'src/testing/owner.service.mock';
+import { AuthService } from '../authentication/auth.service';
+import { MockAuthService } from 'src/testing/auth.service.mock';
 
-// describe('AddNoteComponent:', () => {
-//   let addNoteComponent: AddNoteComponent;
-//   let addNoteForm: FormGroup;
-//   let calledClose: boolean;
-//   let fixture: ComponentFixture<AddNoteComponent>;
-//   let mockOwnerService = MockOwnerService;
+describe('AddNoteComponent:', () => {
+  let addNoteComponent: AddNoteComponent;
+  let addNoteForm: FormGroup;
+  let calledClose: boolean;
+  let fixture: ComponentFixture<AddNoteComponent>;
+  let mockOwnerService = MockOwnerService;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       imports: [
-//         FormsModule,
-//         ReactiveFormsModule,
-//         MatSnackBarModule,
-//         MatCardModule,
-//         MatFormFieldModule,
-//         MatSelectModule,
-//         MatInputModule,
-//         BrowserAnimationsModule,
-//         RouterTestingModule
-//       ],
-//       declarations: [AddNoteComponent],
-//       providers: [{ provide: NotesService, useValue: new MockNoteService()},
-//          {provide: OwnerService, useValue: new MockOwnerService()} ]
-//     }).compileComponents().catch(error => {
-//       expect(error).toBeNull();
-//     });
-//   }));
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        MatSnackBarModule,
+        MatCardModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        MatInputModule,
+        BrowserAnimationsModule,
+        RouterTestingModule,
+      ],
+      declarations: [AddNoteComponent],
+      providers: [
+        { provide: NotesService, useValue: new MockNoteService() },
+        { provide: OwnerService, useValue: new MockOwnerService() },
+        { provider: AuthService, useValue: new MockAuthService() },
+      ],
+    }).compileComponents().catch(error => {
+      expect(error).toBeNull();
+    });
+  });
 
-//   beforeEach(() => {
-//     calledClose = false;
-//     fixture = TestBed.createComponent(AddNoteComponent);
-//     addNoteComponent = fixture.componentInstance;
-//     addNoteComponent.ngOnInit();
-//     fixture.detectChanges();
-//     addNoteForm = addNoteComponent.addNoteForm;
-//     expect(addNoteForm).toBeDefined();
-//     expect(addNoteForm.controls).toBeDefined();
-//   });
+  beforeEach(() => {
+    calledClose = false;
+    fixture = TestBed.createComponent(AddNoteComponent);
+    addNoteComponent = fixture.componentInstance;
+    addNoteComponent.ngOnInit();
+    fixture.detectChanges();
+    addNoteForm = addNoteComponent.addNoteForm;
+  });
 
-//   it('should create the component and form', () => {
-//     expect(addNoteComponent).toBeTruthy();
-//     expect(addNoteForm).toBeTruthy();
-//   });
+  it('should create', () => {
+    expect(addNoteComponent).toBeTruthy();
+  });
 
-//   it('form should be invalid when empty', () => {
-//     expect(addNoteForm.valid).toBeFalsy();
-//   });
+  describe('The addNoteForm:', () => {
+    it('should create', () => {
+      expect(addNoteForm).toBeDefined();
+      expect(addNoteForm.controls).toBeDefined();
+    });
+
+    it('should be invalid when empty', () => {
+      expect(addNoteForm.valid).toBeFalsy();
+    });
+  });
 
 //   describe('The body field:', () => {
 //     let bodyControl: AbstractControl;
@@ -91,5 +100,4 @@
 //       expect(bodyControl.hasError('maxlength')).toBeTruthy();
 //     });
 //   });
-
-// });
+});
