f0e444c9e8502f99c46903b716e33d56d2a1e507
Johannes Martinez <mart4695@morris.umn.edu>
2020-04-28 14:07:02 -0500

    Implement new note type

--- client/src/app/note.ts
+++ client/src/app/note.ts
@@ -1,8 +1,18 @@
-export interface Note {
+export interface Note extends NewNote {
   _id: string;
-  owner_id: string;
-  body: string;
   addDate: Date;
-  expireDate: Date;
-  status: string;
 }
+
+
+/**
+ * When we create a new note, not all of the fields exist yet. Some of
+ * them are left for the server to fill in.
+ */
+export interface NewNote {
+  body: string;
+  expireDate: Date;
+  status: NoteStatus;
+}
+
+
+export type NoteStatus = 'active' | 'deleted';
