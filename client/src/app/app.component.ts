4ac0cfe0bb92aa24e25f5860c0eee13a91eaf036
Joe Walbran <walbr037@morris.umn.edu>
2020-04-30 22:22:59 -0500

    Make the viewer mode in the menu work
    In order to make this link work, we need to get the user's x500 from
    Auth0 in app.component.ts.
    
    (Sorry, I missed that one earlier!)

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AuthService } from './authentication/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'DoorBoard';
+  x500: string;
+
+  constructor(public auth: AuthService) {}
+
+  ngOnInit() {
+    this.auth.getUser$().subscribe(user => {
+      this.x500 = user.nickname;
+    });
+  }
+
+  getViewerLink() {
+    return '/' + this.x500;
+  }
 }
