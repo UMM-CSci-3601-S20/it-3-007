ae2d1b703578a2bc45c2d389efae90d16beca00e
Jake <jake.peterson7@aol.com>
2020-05-02 15:17:02 -0500

    Add copyURL() to owner.component.ts
    This doesn't work correctly right now, but that's okay because it doesn't belong here and I'm taking it out eventually. However, I want it here for now.

--- client/src/app/owner/owner.component.ts
+++ client/src/app/owner/owner.component.ts
@@ -71,6 +71,21 @@ export class OwnerComponent implements OnInit, AfterViewInit {
     });
   }
 
+  copyURL(): void {
+    const val = window.location.href  + this.x500;
+    const selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = val;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+  }
+
   openExternalLink(url: string) {
     // We can't use window.open(url, '_blank') here, because Safari
     // doesn't like that.
