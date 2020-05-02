82eaa4069d04dc9d24a2b1a7bf936c921c5a7ce7
Jake <jake.peterson7@aol.com>
2020-05-02 15:08:29 -0500

    Add in the copyURL() function from owner.component.ts
    Originally the share menu was in the wrong place so this also needs to be moved over.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -23,4 +23,18 @@ export class AppComponent implements OnInit {
   getViewerLink() {
     return '/' + this.x500;
   }
+  copyURL(): void {
+    const val = window.location.href  + this.x500;
+    const selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = val;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+  }
 }
