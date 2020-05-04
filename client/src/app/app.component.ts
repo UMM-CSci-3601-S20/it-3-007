25f557abfe6c0ccec9bbc727e460652cc7ad7719
Joe Walbran <walbr037@morris.umn.edu>
2020-05-04 17:43:53 -0500

    Make AppComponent get the user JIT instead of in ngOnInit()
    If AppComponent only reads the user's fields when necessary, we'll be able to use AppComponent when the user is null (that is, when the user is not logged in).
    
    By contrast, if AppComponent tries to read the user's fields in its ngOnInit method, it will immediately throw an error if the user isn't logged in.
    
    (We could also use the safe navigation operator `?.`, in ngOnInit() to produce roughly the same behavior. ngOnInit() wouldn't throw any null errors, but the rest of the methods still might.
    
    All else being equal, I would prefer to keep the null propagation to a minimum, so I'm avoiding the safe navigation operator in favor of not-reading-those-fields-right-now.)

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { take } from 'rxjs/operators';
 import { AuthService } from './authentication/auth.service';
 import { DOCUMENT } from '@angular/common';
 import { OwnerService } from './owner.service';
 import { environment } from 'src/environments/environment';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -13,43 +13,50 @@ import { environment } from 'src/environments/environment';
 })
 export class AppComponent implements OnInit {
   title = 'DoorBoard';
-  x500: string;
-  name: string;
 
   constructor(
     public auth: AuthService,
     @Inject(DOCUMENT) private document: Document,
-    private ownerService: OwnerService) { }
+    private router: Router,
+    private ownerService: OwnerService,
+  ) {}
 
-  ngOnInit() {
-    this.auth.getUser$().subscribe(user => {
-      this.x500 = user.nickname;
-      this.name = user.name;
+  ngOnInit() {}
+
+  openSign(): void {
+    this.auth.getUser$().pipe(
+      take(1),
+    ).subscribe(user => {
+      const signUrl = this.ownerService.getSignUrl(user.name, user.nickname);
+      this.openExternalLink(signUrl);
     });
   }
 
-  getViewerLink() {
-    return '/' + this.x500;
-  }
-
-  openSign(): void {
-    this.openExternalLink(
-      this.ownerService.getSignUrl(this.name, this.x500));
-  }
-
   copyURL(): void {
-    const val = environment.BASE_URL + '/' + this.x500;
-    const selBox = document.createElement('textarea');
-    selBox.style.position = 'fixed';
-    selBox.style.left = '0';
-    selBox.style.top = '0';
-    selBox.style.opacity = '0';
-    selBox.value = val;
-    document.body.appendChild(selBox);
-    selBox.focus();
-    selBox.select();
-    document.execCommand('copy');
-    document.body.removeChild(selBox);
+    this.auth.getUser$().pipe(
+      take(1),
+    ).subscribe(user => {
+      const val = environment.BASE_URL + '/' + user.nickname;
+      const selBox = document.createElement('textarea');
+      selBox.style.position = 'fixed';
+      selBox.style.left = '0';
+      selBox.style.top = '0';
+      selBox.style.opacity = '0';
+      selBox.value = val;
+      document.body.appendChild(selBox);
+      selBox.focus();
+      selBox.select();
+      document.execCommand('copy');
+      document.body.removeChild(selBox);
+    });
+  }
+
+  goToViewerPage() {
+    this.auth.getUser$().pipe(
+      take(1),
+    ).subscribe(user => {
+      this.router.navigate([`/${user.nickname}`]);
+    });
   }
 
   openExternalLink(url: string) {
