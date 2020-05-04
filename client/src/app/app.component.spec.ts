ed559c62807a508e90ca977f21b33c70a8575d5b
Joe Walbran <walbr037@morris.umn.edu>
2020-05-04 14:27:49 -0500

    Make the Karma tests pass
    Since sign generation is now handled by AppComponent, we moved those tests from owner.component.spec.ts to app.component.spec.ts
    
    (They also needed a bit of adjustment to work with the new Material menus.)

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,8 +1,7 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { AppModule } from './app.module';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatCardModule } from '@angular/material/card';
@@ -11,10 +10,17 @@ import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
 import { AuthService } from './authentication/auth.service';
 import { MockAuthService, professorJohnson } from 'src/testing/auth.service.mock';
+import { OwnerService } from './owner.service';
+import { MockOwnerService } from 'src/testing/owner.service.mock';
+import { By } from '@angular/platform-browser';
+
 
 describe('AppComponent:', () => {
-  let fixture;
-  let app;
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
+  let generateSignButton: HTMLElement;
+  const mockOwnerService = new MockOwnerService();
+  const mockAuthService = new MockAuthService();
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -32,7 +38,8 @@ describe('AppComponent:', () => {
         AppComponent
       ],
       providers: [
-        { provide: AuthService, useValue: new MockAuthService() },
+        { provide: AuthService, useValue: mockAuthService },
+        { provide: OwnerService, useValue: mockOwnerService },
       ],
     }).compileComponents();
   }));
@@ -41,6 +48,20 @@ describe('AppComponent:', () => {
     fixture = TestBed.createComponent(AppComponent);
     app = fixture.componentInstance;
     app.ngOnInit();
+  }));
+
+  beforeEach(async(() => {
+    // Open the share menu.
+    fixture.detectChanges();
+    generateSignButton = fixture.debugElement
+      .query(By.css('.share-button'))
+      .nativeElement
+      .click();
+
+    fixture.detectChanges();
+    generateSignButton = fixture.debugElement
+      .query(By.css('#generate-pdf-button'))
+      .nativeElement;
   }));
 
   it('creates the app', () => {
@@ -55,5 +76,53 @@ describe('AppComponent:', () => {
     it('gives the right url for the viewer page', () => {
       expect(app.getViewerLink()).toEqual(`/${professorJohnson.nickname}`);
     });
-  })
+  });
+
+  describe('Making the sign:', () => {
+    const pretendPdfUrl = 'pretend url';
+
+    beforeEach(() => {
+      spyOn(mockOwnerService, 'getSignUrl').and.returnValue(pretendPdfUrl);
+
+      spyOn(app, 'openExternalLink');
+    });
+
+    describe('The openPDF() method:', () => {
+      it('gets a pdf document from OwnerService', async(() => {
+        app.openPDF();
+
+        fixture.whenStable().then(() => {
+          expect(mockOwnerService.getSignUrl).toHaveBeenCalledTimes(1);
+        });
+      }));
+
+      it('opens that pdf', async(() => {
+        app.openPDF();
+
+        fixture.whenStable().then(() => {
+          expect(app.openExternalLink)
+            .toHaveBeenCalledWith(pretendPdfUrl);
+        });
+      }));
+    });
+
+    describe('The GENERATE SIGN button:', () => {
+      it('gets a pdf document from OwnerService', async(() => {
+        generateSignButton.click();
+
+        fixture.whenStable().then(() => {
+          expect(mockOwnerService.getSignUrl).toHaveBeenCalledTimes(1);
+        });
+      }));
+
+      it('opens that pdf', async(() => {
+        generateSignButton.click();
+
+        fixture.whenStable().then(() => {
+          expect(app.openExternalLink)
+            .toHaveBeenCalledWith(pretendPdfUrl);
+        });
+      }));
+    });
+  });
 });
